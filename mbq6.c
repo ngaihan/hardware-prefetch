b11036a545cea24592398716d58020dec64be9fa
michaeljsXu <michaelx.0277@gmail.com>
2024-11-20 01:34:52 -0500

    commented mbq6

--- mbq6.c
+++ mbq6.c
@@ -1,15 +1,26 @@
 #define LOOP_SIZE 1000000000
 
-int main() {
+int main()
+{
+    // initialize array
     int a[LOOP_SIZE];
+    // initial index is 0
     register int index = 0;
+    // jumps array is used to determine the next jump value
+    // this will ensure that jump oscillates between 2 and 4
     int jumps[10] = {0, 0, 4, 0, 2};
+    // this is how much index will be incremented by
     register int jump = 2;
+    // b
     register int b;
-    while (index < LOOP_SIZE) {
+    while (index < LOOP_SIZE)
+    {
+        // access a at index
         b = a[index];
-        a[index] = b+1;
+        a[index] = b + 1;
+        // this ensures that the next index will be outside of the current block
         index += jump * 64;
+        // get the next jump value
         jump = jumps[jump];
     }
 }
