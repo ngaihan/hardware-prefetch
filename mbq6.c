19a5ca3d8ae11854aeef1db7f7e423b30620f3f6
michaeljsXu <michaelx.0277@gmail.com>
2024-11-20 01:35:31 -0500

    additional comments for mbq6

--- mbq6.c
+++ mbq6.c
@@ -8,6 +8,7 @@ int main()
     register int index = 0;
     // jumps array is used to determine the next jump value
     // this will ensure that jump oscillates between 2 and 4
+    // this means that stride prefetcher should fail to fetch the correct data
     int jumps[10] = {0, 0, 4, 0, 2};
     // this is how much index will be incremented by
     register int jump = 2;
@@ -18,7 +19,8 @@ int main()
         // access a at index
         b = a[index];
         a[index] = b + 1;
-        // this ensures that the next index will be outside of the current block
+        // this ensures that the next index will be outside of the next block
+        // this ensures that nextline prefetcher will fail to get the correct data
         index += jump * 64;
         // get the next jump value
         jump = jumps[jump];
